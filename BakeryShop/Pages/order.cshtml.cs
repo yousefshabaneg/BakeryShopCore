764ff4b423f99557143e09b35d0aa7fa416fb345
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-07-12 23:10:01 +0200

    Order Page

--- BakeryShop/Pages/order.cshtml.cs
+++ BakeryShop/Pages/order.cshtml.cs
@@ -1,16 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BakeryShop.Data;
+using BakeryShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Threading.Tasks;
 
 namespace BakeryShop.Pages
 {
     public class orderModel : PageModel
     {
-        public void OnGet()
+        private readonly BakeryShopContext _db;
+        public orderModel(BakeryShopContext db) => _db = db;
+        public Product Product { get; set; }
+        [BindProperty(SupportsGet =true)]
+        public int Id { get; set; }
+
+        public async Task OnGetAsync()
         {
+            Product = await _db.Products.FindAsync(Id);
         }
     }
 }
