151800d13934843e455c40cc892cbe0d41b2c693
Yousef Shaban <yousefshaban.eg@gmail.com>
2021-07-12 22:55:36 +0200

    Index Page

--- BakeryShop/Pages/Index.cshtml.cs
+++ BakeryShop/Pages/Index.cshtml.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BakeryShop.Data;
+using BakeryShop.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -10,16 +12,23 @@ namespace BakeryShop.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly BakeryShopContext _db;
+
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, BakeryShopContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public void OnGet()
-        {
+        public List<Product> Products { get; set; } = new List<Product>();
+        public Product FeaturedProduct { get; set; }
 
+        public async Task OnGetAsync()
+        {
+            Products = await _db.Products.ToListAsync();
+            FeaturedProduct = Products.ElementAt(new Random().Next(Products.Count()));
         }
     }
 }
